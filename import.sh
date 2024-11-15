41495da686243b16f0df6adca2663ba75e40c9d5
main() <main@ehvag.eu.org>
2024-11-15 13:47:32 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -13,9 +13,13 @@ BQ_LOCATION='europe-north1'
 
 echo "Exporting data from Firestore ..."
 export_prefix="$(gcloud firestore export "$BUCKET" "--collection-ids=$FIRESTORE_COLLECTIONS" "--database=$FIRESTORE_DB" --format=json | jq -r '.metadata.outputUriPrefix')"
+[[ -z "$export_prefix" ]] && exit 1
 
 for coll in ${FIRESTORE_COLLECTIONS//,/ }
 do
     echo "Importing '${coll}' to BigQuery ..."
     bq load --replace --source_format=DATASTORE_BACKUP "${BQ_DATASET}.${coll}" "${export_prefix}/all_namespaces/kind_${coll}/all_namespaces_kind_${coll}.export_metadata"
 done
+
+echo "Cleaning up temporary files ..."
+gcloud storage rm --recursive "$export_prefix"
